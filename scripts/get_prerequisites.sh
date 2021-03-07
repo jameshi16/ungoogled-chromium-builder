c08c96267bbb818ad70206a411f275cf422803a2
James <lim.en.hui.james@gmail.com>
2021-03-07 13:27:50 +0800

    Remove trailing whitespace

--- scripts/get_prerequisites.sh
+++ scripts/get_prerequisites.sh
@@ -12,16 +12,4 @@ $APT_INSTALL git equivs python3 packaging-dev wget software-properties-common uu
 # ./llvm.sh 9
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
-# LLVM
-# CLANG_PACKAGES=($($APT_SEARCH clang | grep -oE "^clang-[0-9]+\s"))
-# HIGHEST_INDEX=0
-# for ((i=0;i<${#CLANG_PACKAGES[@]};i++));
-# do
-#	HIGHEST_VERSION=$(echo ${CLANG_PACKAGES[$HIGHEST_INDEX]} | grep -oE "[0-9]+")
-#	CURRENT_VERSION=$(echo ${CLANG_PACKAGES[$i]} | grep -oE "[0-9]+")
-#	if [[ "$CURRENT_VERSION" -ge "$HIGHEST_VERISON" ]]; then
-#		HIGHEST_INDEX=$i
-#	fi
-# done
-
 # $APT_INSTALL ${CLANG_PACKAGES[$HIGHEST_INDEX]}
